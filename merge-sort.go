72250b88759ab04bfd62ed2f47f8c8dd5e940dc4
Gowtham Prudhvi <v.g.prudhvi38@gmail.com>
2020-02-26 20:28:29 +0530

    add basic structure

--- merge-sort.go
+++ merge-sort.go
@@ -1,7 +1,26 @@
 package main
 
 import "fmt"
+func merge(arr []int, l int, r int, mid int) {
+	return
+}
+
+func mergeSort(arr []int, l int, r int) {
+	var mid int = (l + r)/2
+	if (l < r) {
+		fmt.Println("middle value ", arr[mid], "mid ", mid)
+		mergeSort(arr, l, mid)
+		mergeSort(arr, mid + 1, r)
+		merge(arr, l, r, mid)
+	}
+	return
+}
 
 func main() {
+	var a [5]int
+	a[4] = 14
+	fmt.Println("a[1] val:", a[1])
+	fmt.Println("a[4] val:", a[4])
 	fmt.Println("Hello World!!!")
+	mergeSort(a[:], 0, len(a) - 1)
 }
