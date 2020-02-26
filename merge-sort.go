410cf4b74a6afb9f99bcd822d0cb198c0fd27a67
Gowtham Prudhvi <v.g.prudhvi38@gmail.com>
2020-02-26 20:46:44 +0530

    add part of merge

--- merge-sort.go
+++ merge-sort.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 func merge(arr []int, l int, r int, mid int) {
+	temp [r - l + 1]int
+	i := 0
+	j := mid
+	k := 0
+	for ; i <= mid && j <= r; {
+		if(arr[i] <= arr[j]) {
+			temp[k] = arr[i]
+			i++
+			k++
+		}
+		else {
+			temp[k] = arr[j]
+			j++
+			k++
+		}
+	}
+	for ; i <= mid; {
+		temp[k] = arr[i]
+		i++
+		k++
+	}
+	for ; j <= r; {
+		temp[k] = arr[j]
+		i++
+		j++
+	}
 	return
 }
 
@@ -18,6 +44,9 @@ func mergeSort(arr []int, l int, r int) {
 
 func main() {
 	var a [5]int
+	a[1] = -1
+	a[2] = 7
+	a[3] = -9
 	a[4] = 14
 	fmt.Println("a[1] val:", a[1])
 	fmt.Println("a[4] val:", a[4])
